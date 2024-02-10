57e825909b6f55b7b89da221f0edb6fc6ceac15d
Yogendra Singh <yogendrasinghhc@gmail.com>
2024-02-10 11:18:35 +0530

    data types in python .....and list datatype and its operation to manipulates and updating the list.

--- FirstDemo.py
+++ FirstDemo.py
@@ -1,5 +1,5 @@
 print("hello")
-#here are the comments i have defined
+# here are the comments i have defined
 
 a = 3
 print(a)
@@ -11,3 +11,25 @@ b,c,d = 5,6.4, "Great"
 
 str = "Hello world"
 print(str)
+# data types in python
+#print("value is" +b)
+print("{} {}". format("value is " , b))
+print(type(b))
+print(type(c))
+print(type(d))
+
+# list datatype and its operation to manipulates
+values = [1,2, "rahul", 4,5]
+# list is the data type that allows multiple values and can be different data types
+print(values[0])    # 1
+print(values[3])    #4
+print(values[-1])   #5
+print(values[1:3])  #2 rahul 4
+values.insert(3, "Shetty")
+print(values)
+values.append("end")
+print(values)
+
+values[2] = "RAHUL" #updating
+del values[0]
+print(values)
